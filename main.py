6afdaac1c7620420a9b1ed122bc620db519c371e
Aryan Adoni <aryanadoni8@gmail.com>
2026-01-30 20:56:59 +0530

    Add logistic regression model training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.data_loader import load_data
 from src.preprocessing import preprocess_data
 from sklearn.model_selection import train_test_split
+from src.model import train_logistic_regression
 
 df = load_data()
 X, y = preprocess_data(df)
@@ -22,3 +23,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print("Training set size:", X_train.shape)
 print("Test set size:", X_test.shape)
+
+
+
+# Train the model
+model = train_logistic_regression(X_train, y_train)
+
+print("Model training completed.")
---
+++ src/model.py
@@ -0,0 +1,7 @@
+from sklearn.linear_model import LogisticRegression
+
+def train_logistic_regression(X_train, y_train):
+
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+    return model
